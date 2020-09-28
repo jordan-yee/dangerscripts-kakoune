da9e80edbcdf1123aee1c39e4ed78b879c323868
Jordan Yee <jordan.yee@mantech.com>
2020-09-28 13:24:31 -0600

    Add add-plugin script (incomplete)

---
+++ add-plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# IN PROGRESS
+
+# This script is used to add a new plugin to this repository,
+# including the submodule and related files.
+
+echo "Enter the plugin's git clone URL:"
+read PLUGIN_CLONE_URL
+
+echo $PLUGIN_CLONE_URL
