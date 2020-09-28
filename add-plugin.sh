96c867f79c321ac006490155087d06286a842080
Jordan Yee <jordan.yee@mantech.com>
2020-09-28 17:14:02 -0600

    Improve robustness of path handling for add-plugin script

--- add-plugin.sh
+++ add-plugin.sh
@@ -1,39 +1,63 @@
 #!/bin/sh
 
-# IN PROGRESS
-
 # This script is used to add a new plugin to this repository,
 # including the submodule and related files.
 
-echo "Enter the plugin's git clone URL:"
-read PLUGIN_URL
+# ------------------------------------------------------------------------------
+# Get plugin and directory info
 
+# If arg is given, assume it's the plugin URL
+# If no args are given, prompt for the URL
+if [ -z "$1" ]; then
+    echo "Enter the URL for the plugin's Git repo:"
+    read PLUGIN_URL
+else
+    PLUGIN_URL=$1
+fi
+
+# Get plugin naming strings
 BASENAME=$(basename $PLUGIN_URL)
-PLUGIN_NAME=${BASENAME%.*}
+PLUGIN_NAME=${BASENAME%.git}
 PLUGIN_FILESAFE_NAME=$(echo $PLUGIN_NAME | tr [:punct:] -)
 
-# Add plugin submodule
-git submodule add $PLUGIN_URL ./plugins/$PLUGIN_NAME
+# Get directory info for this script
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
+# ------------------------------------------------------------------------------
+# Add plugin submodule
+
+cd $SCRIPT_DIR
+git submodule add $PLUGIN_URL $SCRIPT_DIR/plugins/$PLUGIN_NAME
+
+# ------------------------------------------------------------------------------
 # Create plugin config file
-PLUGIN_CONFIG_FILE=./plugin-configs/$PLUGIN_FILESAFE_NAME-config.kak
+
+mkdir $SCRIPT_DIR/plugin-configs
+PLUGIN_CONFIG_FILE=$SCRIPT_DIR/plugin-configs/$PLUGIN_FILESAFE_NAME-config.kak
+echo "Creating $PLUGIN_CONFIG_FILE..."
 cat << EOF > $PLUGIN_CONFIG_FILE
 # Configuration for the \`$PLUGIN_NAME\` plugin
+# ${PLUGIN_URL%.git}
 
 # TODO: Add configuration here
 EOF
 
+# ------------------------------------------------------------------------------
 # Create plugin install file
-PLUGIN_INSTALL_FILE=./plugin-installs/$PLUGIN_FILESAFE_NAME-install.sh
+
+mkdir $SCRIPT_DIR/plugin-installs
+PLUGIN_INSTALL_FILE=$SCRIPT_DIR/plugin-installs/$PLUGIN_FILESAFE_NAME-install.sh
+echo "Creating $PLUGIN_INSTALL_FILE..."
 cat << EOF > $PLUGIN_INSTALL_FILE
 #!/bin/sh
 
 mkdir -pv ~/.config/kak/plugins/$PLUGIN_NAME
 
-# TODO: cp relevant files into plugin directory created above
-cp -iv ../plugins/$PLUGIN_NAME ~/.config/kak/plugins/$PLUGIN_NAME
-EOF
+SCRIPT_PATH=\$(realpath \$0)
+SCRIPT_DIR=\$(dirname \$SCRIPT_PATH)
+REPO_ROOT_PATH=\$(dirname \$SCRIPT_DIR)
 
-# HOLD OPEN FOR DEVELOPMENT
-# echo "Press enter to exit..."
-# read FOO
+# TODO: cp relevant files into plugin directory created above
+cp -irv \$REPO_ROOT_PATH/plugins/$PLUGIN_NAME ~/.config/kak/plugins/
+EOF
