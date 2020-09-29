4a8d4948dafc70ce959e2ec9926c44d3da448e8e
Jordan Yee <jordan.yee@mantech.com>
2020-09-28 20:58:57 -0600

    Remove TODO comment from smarttab.kak install script

--- plugin-installs/smarttab-kak-install.sh
+++ plugin-installs/smarttab-kak-install.sh
@@ -6,5 +6,4 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
 
-# TODO: cp relevant files into plugin directory created above
 cp -irv $REPO_ROOT_PATH/plugins/smarttab.kak ~/.config/kak/plugins/
