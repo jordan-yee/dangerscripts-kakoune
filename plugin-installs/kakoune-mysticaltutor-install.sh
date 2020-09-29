1e9e1001dbb7dc2546b7d2ff3dee35da76dc8e90
Jordan Yee <jordan.yee@mantech.com>
2020-09-29 13:11:40 -0600

    Fix paths in mysticaltutor install script

--- plugin-installs/kakoune-mysticaltutor-install.sh
+++ plugin-installs/kakoune-mysticaltutor-install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Get directory info for this script
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
+
+# Ensure required directories exist
 mkdir -pv ~/.config/kak/colors
 
-cp ../plugins/kakoune-mysticaltutor/mysticaltutor.kak ~/.config/kak/colors/
+# Copy theme to Kakoune config directory
+cp -iv $REPO_ROOT_PATH/plugins/kakoune-mysticaltutor/mysticaltutor.kak ~/.config/kak/colors/
