7127c6fc30b2e1fcc820eb7577e6ff4fd1f4a315
Jordan Yee <jordan.yee@mantech.com>
2020-09-29 16:33:32 -0600

    Update install/config files for case.kak plugin

--- plugin-configs/case-kak-config.kak
+++ plugin-configs/case-kak-config.kak
@@ -1,4 +1,6 @@
 # Configuration for the `case.kak` plugin
 # https://gitlab.com/FlyingWombat/case.kak
 
+source ~/.config/kak/plugins/case.kak/case.kak
+
 map global normal '`' ': enter-user-mode case<ret>'
--- plugin-installs/case-kak-install.sh
+++ plugin-installs/case-kak-install.sh
@@ -6,4 +6,4 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
 
-cp -irv $REPO_ROOT_PATH/plugins/case.kak ~/.config/kak/plugins/
+cp -iv $REPO_ROOT_PATH/plugins/case.kak/rc/case.kak ~/.config/kak/plugins/case.kak/
