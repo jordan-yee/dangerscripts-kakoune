15d0f073d953aba0ced411021ef513c2b170f615
Jordan Yee <jordan.yee@mantech.com>
2020-09-29 17:20:29 -0600

    Update install/config files for prelude.kak plugin

--- plugin-configs/prelude-kak-config.kak
+++ plugin-configs/prelude-kak-config.kak
@@ -1,4 +1,6 @@
 # Configuration for the `prelude.kak` plugin
 # https://github.com/alexherbo2/prelude.kak
 
+source ~/.config/kak/plugins/prelude.kak/rc/prelude.kak
+
 require-module prelude
--- plugin-installs/prelude-kak-install.sh
+++ plugin-installs/prelude-kak-install.sh
@@ -6,4 +6,4 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
 
-cp -irv $REPO_ROOT_PATH/plugins/prelude.kak/rc ~/.config/kak/plugins/prelude.kak
+cp -irv $REPO_ROOT_PATH/plugins/prelude.kak/rc ~/.config/kak/plugins/prelude.kak/
