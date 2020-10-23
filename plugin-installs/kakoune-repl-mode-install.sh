87bb8d260f112fd5f38994440598fec84ccafe69
Jordan Yee <jordan.yee@mantech.com>
2020-10-23 14:22:55 -0600

    Fix kakoune-repl-mode-install script

--- plugin-installs/kakoune-repl-mode-install.sh
+++ plugin-installs/kakoune-repl-mode-install.sh
@@ -6,4 +6,4 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
 
-cp -iv $REPO_ROOT_PATH/plugins/kakoune-repl-mode/repl-mode.kak ~/.config/kak/plugins/kakoune-repl-mode/
+cp -irv $REPO_ROOT_PATH/plugins/kakoune-repl-mode ~/.config/kak/plugins/
