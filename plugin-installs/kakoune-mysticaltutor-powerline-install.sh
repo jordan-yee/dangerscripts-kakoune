3db20c9df595f1899778e6286783b24a5ff0ae2e
Jordan Yee <jyee117@gmail.com>
2020-12-30 11:41:51 -0700

    Configure kakoune-mysticaltutor-powerline-install file

--- plugin-installs/kakoune-mysticaltutor-powerline-install.sh
+++ plugin-installs/kakoune-mysticaltutor-powerline-install.sh
@@ -6,5 +6,4 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 REPO_ROOT_PATH=$(dirname $SCRIPT_DIR)
 
-# TODO: cp relevant files into plugin directory created above
-cp -irv $REPO_ROOT_PATH/plugins/kakoune-mysticaltutor-powerline ~/.config/kak/plugins/
+cp -iv $REPO_ROOT_PATH/plugins/kakoune-mysticaltutor-powerline/mysticaltutor-powerline.kak ~/.config/kak/plugins/kakoune-mysticaltutor-powerline/
